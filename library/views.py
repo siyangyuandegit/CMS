2a036f3d5ede572d574b13f87bb9b84cdd6f36e5
jdl123 <1003581546@qq.com>
2018-10-31 19:59:11 +0800

    完成一亿个功能

--- library/views.py
+++ library/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from index.models import *
 
 # Create your views here.
 def bookInfo(request):
@@ -6,7 +8,26 @@ def bookInfo(request):
 
 
 def bookMaintain(request):
-    return render(request,'bookMaintain.html')
+    if request.method =='GET':
+        return render(request,'bookMaintain.html')
+    else:
+        re = request.POST.get('select')
+        if re =='bookid':
+            id = request.POST.get('info')
+            id=int(id)
+            try :
+                 all = Bookinfo.objects.get(bid=id)
+                 return render(request,'bookMaintain.html',{'all':all})
+            except:
+                return HttpResponse('没有这本书')
+        else:
+            name = request.POST.get('info')
+            try :
+                all = Bookinfo.objects.get(bname=name)
+                return render(request, 'bookMaintain.html', {'all': all})
+            except:
+                return HttpResponse('没有这本书')
+
 
 
 def bookBorrow(request):
@@ -19,3 +40,20 @@ def bookReturn(request):
 
 def borrowQuery(request):
     return render(request,'borrowQuery.html')
+
+
+def bookupdate(request,num):
+    if request.method =='GET':
+        all = Bookinfo.objects.get(bid=num)
+        return render(request,'bookupdate.html',{'all':all})
+    else:
+        bookid = request.POST.get('bookid')
+        bookname = request.POST.get('bookname')
+        bpub = request.POST.get('bookp')
+        bprice = request.POST.get('bookprice')
+        bn = request.POST.get('booknum')
+        try:
+            Bookinfo.objects.filter(bid=num).update(bid=bookid,bname=bookname,publication=bpub,price=bprice,bnum=bn)
+            return HttpResponse('更改成功')
+        except:
+            return HttpResponse('输入有误')
